fe36a30f0d0b583b53296495308e6455e12b184a
xenoteo <fedorovxa@gmail.com>
2020-12-13 23:15:01 +0100

    Inputs moved to another directory

--- src/xenoteo/com/github/day7/part2/Main.java
+++ src/xenoteo/com/github/day7/part2/Main.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Map<String, HashMap<String, Integer>> rules = new InputReader()
-                .readInputFileIntoRuleMapWithNumbers("/home/xeno/xWs/Java/Advent-2020/src/xenoteo/com/github/day7/input.txt");
+                .readInputFileIntoRuleMapWithNumbers("/home/xeno/xWs/Java/Advent-2020/src/xenoteo/com/github/day7/input/input.txt");
         System.out.println(new Solution().countBagsContainedByColor(rules, "shiny gold"));
     }
 }
