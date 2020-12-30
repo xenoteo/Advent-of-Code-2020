32b985bedf8f87f3c7d67b4877294a56248fb0d5
xenoteo <fedorovxa@gmail.com>
2020-12-30 11:13:15 +0100

    Day 17 part 2

--- src/xenoteo/com/github/day17/Coordinates.java
+++ src/xenoteo/com/github/day17/Coordinates.java
@@ -3,17 +3,27 @@ package xenoteo.com.github.day17;
 import java.util.Objects;
 
 /**
- * Class representing 3D point coordinates.
+ * Class representing either 3D or 4D point coordinates.
+ * (In case of 3D the fourth value set to 0 as default.)
  */
 public class Coordinates {
     public int x;
     public int y;
     public int z;
+    public int w;
 
     public Coordinates(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.w = 0;
+    }
+
+    public Coordinates(int x, int y, int z, int w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
     }
 
     @Override
@@ -21,11 +31,11 @@ public class Coordinates {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Coordinates that = (Coordinates) o;
-        return x == that.x && y == that.y && z == that.z;
+        return x == that.x && y == that.y && z == that.z && w == that.w;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(x, y, z);
+        return Objects.hash(x, y, z, w);
     }
 }
--- src/xenoteo/com/github/day17/part2/Solution.java
+++ src/xenoteo/com/github/day17/part2/Solution.java
@@ -1,4 +1,4 @@
-package xenoteo.com.github.day17.part1;
+package xenoteo.com.github.day17.part2;
 
 import xenoteo.com.github.day17.Coordinates;
 
@@ -6,13 +6,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * The pocket dimension contains an infinite 3-dimensional grid. At every integer 3-dimensional coordinate (x,y,z),
- * there exists a single cube which is either active or inactive.
+ * The pocket dimension contains an infinite 4-dimensional grid. At every integer 4-dimensional coordinate (x,y,z,w),
+ * there exists a single cube (really, a hypercube) which is either active or inactive.
  *
  * In the initial state of the pocket dimension, almost all cubes start inactive. The only exception to this is a small
  * flat region of cubes; the cubes in this region start in the specified active (#) or inactive (.) state.
  *
- * Each cube only ever considers its neighbors: any of the 26 other cubes where any of their coordinates differ by at most 1.
+ * Each cube only ever considers its neighbors: any of the 80 other cubes where any of their coordinates differ by at most 1.
  *
  * During a cycle, all cubes simultaneously change their state according to the following rules:
  * - If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active.
@@ -20,7 +20,7 @@ import java.util.Set;
  * - If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active.
  *   Otherwise, the cube remains inactive.
  *
- * Even though the pocket dimension is 3-dimensional, this initial state represents a small 2-dimensional slice of it.
+ * Even though the pocket dimension is 4-dimensional, this initial state represents a small 2-dimensional slice of it.
  *
  * Counting how many cubes are left in the active state after the sixth cycle.
  */
@@ -28,7 +28,7 @@ public class Solution {
 
     /**
      * Counts how many cubes are left in the active state after the sixth cycle.
-     * @param states initial state (2D slice of 3D where z = 0)
+     * @param states initial state (2D slice of 4D where z = 0, w = 0)
      * @return number of active cubes after 6 cycles
      */
     public int activeCubesAfter6Cycles(char[][] states){
@@ -66,7 +66,7 @@ public class Solution {
         for (int i = 0; i < states.length; i++){
             for (int j = 0; j < states[0].length; j++){
                 if (states[i][j] == '#')
-                    activeCubes.add(new Coordinates(i, j, 0));
+                    activeCubes.add(new Coordinates(i, j, 0, 0));
             }
         }
         return activeCubes;
@@ -94,8 +94,10 @@ public class Solution {
         for (int dx = - 1; dx <= 1; dx++){
             for (int dy = - 1; dy <= 1; dy++){
                 for (int dz = - 1; dz <= 1; dz++){
-                    if (!(dx == 0 && dy == 0 && dz == 0)){
-                        neighbours.add(new Coordinates(cube.x + dx, cube.y + dy, cube.z + dz));
+                    for (int dw = - 1; dw <= 1; dw++){
+                        if (!(dx == 0 && dy == 0 && dz == 0 && dw == 0)){
+                            neighbours.add(new Coordinates(cube.x + dx, cube.y + dy, cube.z + dz, cube.w + dw));
+                        }
                     }
                 }
             }
@@ -113,10 +115,12 @@ public class Solution {
         for (int dx = - 1; dx <= 1; dx++){
             for (int dy = - 1; dy <= 1; dy++){
                 for (int dz = - 1; dz <= 1; dz++){
-                    if (!(dx == 0 && dy == 0 && dz == 0)){
-                        if (activeCubes.contains(
-                                new Coordinates(cube.x + dx, cube.y + dy, cube.z + dz)))
-                            activeNeighbours++;
+                    for (int dw = - 1; dw <= 1; dw++){
+                        if (!(dx == 0 && dy == 0 && dz == 0 && dw == 0)){
+                            if (activeCubes.contains(
+                                    new Coordinates(cube.x + dx, cube.y + dy, cube.z + dz, cube.w + dw)))
+                                activeNeighbours++;
+                        }
                     }
                 }
             }
